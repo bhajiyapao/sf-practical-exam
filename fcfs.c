91661f80eb3684b86a1320b55b6ff1e846824d59
hideo23kojima <153442787+hideo23kojima@users.noreply.github.com>
2023-12-10 20:39:44 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -12,7 +12,6 @@ int temporary[size];
 void FCFS(int cylinders[], int temporary[], int cylinder_size, int request, int seektime);
 
 void main() {
-    printf("\t-----RONITSATISHMEHTA 60009230207-------\n");
     printf("\t--------FCFS SIMULATION----------------\n");
     printf("\nEnter the size of disk:");
     scanf("%d", &cylinder_size);
