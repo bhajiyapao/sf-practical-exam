077c0b82bb38f76ac982a7c8adf2e686f261203d
hideo23kojima <153442787+hideo23kojima@users.noreply.github.com>
2023-12-10 21:01:54 +0530

    Update SRTF.c

--- SRTF.c
+++ SRTF.c
@@ -8,7 +8,6 @@ void main(){
     int process[size];
     int n;
     printf("\tWELCOME TO SRTF CALCULATOR\t \n");
-    printf("\nRonit Satish Mehta 60009230207\n");
     printf("\nEnter the number of process:\n");
     scanf("%d",&n);
     printf("\nEnter the process number:\n");
