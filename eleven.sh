445a700b27367632f8e34713b9e65e8838767de2
kojimbo <153442787+kojimbo-23@users.noreply.github.com>
2023-12-13 18:21:43 +0530

    Update eleven.sh

--- eleven.sh
+++ eleven.sh
@@ -7,12 +7,10 @@ count_vowels() {
     echo "$1" | tr -cd 'aeiouAEIOU' | wc -c
 }
 
-val=1
 
-if ((val == 0)); then
+if [ "$pid" -eq 1 ]; then
     echo "$(echo "$sentence" | wc -w)"
 else
-    val=0
-    export val
+    wait $pid
     echo "$(count_vowels "$sentence")"
 fi
